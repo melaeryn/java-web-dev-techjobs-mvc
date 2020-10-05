2e2bcb417309b70808ec233e639f57156061c4cb
Marlaina <mlwunder@mtu.edu>
2020-10-05 17:04:35 -0500

    Finished Up Main Assignment

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -25,4 +25,18 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String searchType){
+        ArrayList<Job> jobs = new ArrayList<>();
+        if(searchTerm.toLowerCase().equals("all") || searchTerm.equals("") ){
+            jobs = JobData.findAll();
+        }
+        else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
 }
